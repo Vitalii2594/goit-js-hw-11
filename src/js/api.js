1df72a49ff2afbb4efc9a3c1cf84027ef9a9e20c
Vitalii Yakubovskyi <123841115+Vitalii2594@users.noreply.github.com>
2023-11-29 21:47:55 +0100

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,29 +1,19 @@
-import axios from 'axios';
-import { API_KEY, BASE_URL, PER_PAGE } from './constants';
-
-async function fetchImages(query, page) {
-  const params = {
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    page: page,
-    per_page: PER_PAGE,
-  };
-
-  try {
-    const response = await axios.get(BASE_URL, { params });
-    const { hits, totalHits } = response.data;
-
-    if (hits.length === 0) {
-      throw new Error('No images found for the given query');
-    }
-
-    return { hits, totalHits }; // Modified to return totalHits along with hits
-  } catch (error) {
-    throw new Error('Error fetching images:', error);
-  }
-}
-
-export { fetchImages };
+import axios from 'axios'; 
+ import { API_KEY, BASE_URL, PER_PAGE } from './constants'; 
+  
+ export async function getImages(page = 1, value) { 
+   const qValue = value; 
+  
+   axios.defaults.baseURL = BASE_URL; 
+   axios.defaults.params = { 
+     key: API_KEY, 
+     q: qValue, 
+     image_type: 'photo', 
+     orientation: 'horizontal', 
+     safesearch: true, 
+     per_page: PER_PAGE, 
+     page: page, 
+   }; 
+   // return await axios.get(BASE_URL); 
+   const { data } = await axios.get(); 
+   return data;
