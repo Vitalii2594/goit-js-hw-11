964a890f399a983cfaa37653d23cc7c82adab984
Vitalii Yakubovskyi <123841115+Vitalii2594@users.noreply.github.com>
2023-11-29 22:47:03 +0100

    scroll

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,6 @@ import { getImages } from './api';
 import createMarkup from '../template/createMarcup';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import { PER_PAGE } from './constants';
 
 const refs = {
@@ -18,27 +17,39 @@ refs.formEl.addEventListener('submit', onSubmitSearch);
 let page = 1;
 let value = '';
 let totalHitsImg = 0;
+let loading = false;
+
+const observer = new IntersectionObserver(
+  async entries => {
+    const entry = entries[0];
+    if (entry.isIntersecting && !loading && page * PER_PAGE < totalHitsImg) {
+      loading = true;
+      await onLoadMore();
+      loading = false;
+    }
+  },
+  { threshold: 1 }
+);
 
 async function onSubmitSearch(e) {
   e.preventDefault();
   value = e.currentTarget.elements.searchQuery.value.trim();
   if (!value) {
-    message('Please write correct data!');
+    message('Будь ласка, введіть правильні дані!');
     return;
   }
   try {
     clearGallery();
     const data = await getImages(page, value);
-    // console.log(data);
     const { hits, total, totalHits } = data;
     if (hits.length === 0) {
       message(
-        'Sorry, there are no images matching your search query. Please try again.'
+        'Вибачте, але зображень, що відповідають вашому запитанню, не знайдено. Будь ласка, спробуйте ще раз.'
       );
       return;
     }
     totalHitsImg = totalHits;
-    messageInfo(`Hooray! We found ${totalHitsImg} images.`);
+    messageInfo(`Ура! Ми знайшли ${totalHitsImg} зображень.`);
     const newGalery = hits.map(el => createMarkup(el)).join('');
     refs.galleryEl.insertAdjacentHTML('beforeend', newGalery);
 
@@ -53,6 +64,7 @@ async function onSubmitSearch(e) {
       refs.loadMore.classList.replace('load-more-hidden', 'load-more');
       refs.loadMore.disabled = false;
       refs.loadMore.addEventListener('click', onLoadMore);
+      observer.observe(refs.loadMore);
       return;
     }
   } catch (error) {
@@ -68,11 +80,17 @@ async function onLoadMore() {
     const data = await getImages(page, value);
     const { hits } = data;
 
+    if (hits.length === 0) {
+      refs.loadMore.classList.replace('load-more', 'load-more-hidden');
+      message('Вибачте, але ви дійшли до кінця результатів пошуку.');
+      observer.unobserve(refs.loadMore);
+      return;
+    }
+
     const newGallery = hits.map(el => createMarkup(el)).join('');
     refs.galleryEl.insertAdjacentHTML('beforeend', newGallery);
 
     refs.loadMore.disabled = false;
-    // lightbox.refresh();
 
     const lightbox = new SimpleLightbox('.gallery a', {
       captions: true,
@@ -83,20 +101,21 @@ async function onLoadMore() {
 
     if (page * PER_PAGE >= totalHitsImg) {
       refs.loadMore.classList.replace('load-more', 'load-more-hidden');
-      message("We're sorry, but you've reached the end of search results.");
+      message('Вибачте, але ви дійшли до кінця результатів пошуку.');
+      observer.unobserve(refs.loadMore);
     }
   } catch (error) {
-    Report.failure('Failed to load more images', '');
+    Report.failure('Не вдалося завантажити більше зображень', '');
     console.error(error);
   }
 }
 
 function message(sms) {
-  Report.warning(`Warning!`, `${sms}`);
+  Report.warning(`Увага!`, `${sms}`);
 }
 
 function messageInfo(sms) {
-  Report.warning(`Info`, `${sms}`);
+  Report.warning(`Інформація`, `${sms}`);
 }
 
 function clearGallery() {
