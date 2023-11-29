8de5e95d050f6e3b62f6c5be394f45c7a754161a
Vitalii Yakubovskyi <123841115+Vitalii2594@users.noreply.github.com>
2023-11-29 22:31:10 +0100

    edd

---
+++ src/template/createMarcup.js
@@ -0,0 +1,30 @@
+export default function createMarkup({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `
+        <div class="photo-card">
+            <a class="gallery__link" href="${largeImageURL}">
+              <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            </a>
+            <div class="info">
+              <p class="info-item">
+                <b>Likes: ${likes}</b>
+              </p>
+              <p class="info-item">
+                <b>Views: ${views}</b>
+              </p>
+              <p class="info-item">
+                <b>Comments: ${comments}</b>
+              </p>
+              <p class="info-item">
+                <b>Downloads: ${downloads}</b>
+              </p>
+            </div>
+          </div>`;
+}
